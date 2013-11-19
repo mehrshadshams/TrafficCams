9100021344863615b905951155939093f20826c6
mehrshadshams <mehrshad.shams@gmail.com>
2013-11-19 09:43:45 +0330

    Initial Commit

---
+++ TrafficCams/Controllers/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrafficCams.Controllers
+{
+    public class CameraController : Controller
+    {
+        //
+        // GET: /Camera/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ TrafficCams/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrafficCams.Startup))]
+namespace TrafficCams
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
